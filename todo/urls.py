56394be0a0da732a5f22f72c70368cbd94040e07
Alexey Ryzhkov <aler32@mail.ru>
2022-12-25 14:44:38 +0300

    Lesson_1 Task_6

--- todo/urls.py
+++ todo/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("api-auth/", include("rest_framework.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+]
--- users/models.py
+++ users/models.py
@@ -1,1 +1,30 @@
-# Create your models here.
+from uuid import uuid4
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.db import models
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username_validator = ASCIIUsernameValidator()
+
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    username = models.CharField(
+        max_length=64,
+        unique=True,
+        help_text=("Required. 64 characters or fewer. Letters, digits and @/./+/-/_ only."),
+        validators=[username_validator],
+        error_messages={
+            "unique": ("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(max_length=64, blank=True)
+    last_name = models.CharField(max_length=64, blank=True)
+    email = models.CharField(
+        max_length=128,
+        unique=True,
+        error_messages={
+            "unique": ("A user with that email address already exists."),
+        },
+    )
