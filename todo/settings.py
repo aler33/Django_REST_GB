0a09ae649893e8dad9a6df714a89a302fd257603
Alexey Ryzhkov <aler32@mail.ru>
2023-01-15 13:41:17 +0000

    Lesson_4 task_1

--- todo/settings.py
+++ todo/settings.py
@@ -149,4 +149,6 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
