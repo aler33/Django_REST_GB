cec4ff073745a55679bcc57efd03b1378f4c8649
Alexey Ryzhkov <aler32@mail.ru>
2023-01-21 23:15:59 +0000

    Lesson_6 Full

--- todo/settings.py
+++ todo/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "todo_work",
     'django_filters',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -162,6 +163,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100
--- todo/urls.py
+++ todo/urls.py
@@ -5,6 +5,11 @@ from rest_framework.routers import DefaultRouter
 from users.views import UserModelViewSet
 from todo_work.views import TodoModelViewSet, ProjectModelViewSet
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 router = DefaultRouter()
 router.register("users", UserModelViewSet)
@@ -16,4 +21,7 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include(router.urls)),   
     path('api-token-auth/', views.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
